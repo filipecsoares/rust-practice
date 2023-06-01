1190e94a07a71d2d90fb15165e4ffd2af6a2327e
filipecsoares <filipecarlossoares@gmail.com>
2023-06-01 08:12:15 -0300

    algorithms: factorial recursive

--- algorithms/factorial/main.rs
+++ algorithms/factorial/main.rs
@@ -9,6 +9,13 @@ fn factorial(num: u32) -> u32 {
     total
 }
 
+fn factorial_recursive(num: u32) -> u32 {
+    if num == 0 || num == 1 {
+        return num;
+    }
+    return num * factorial_recursive(num - 1);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -21,5 +28,15 @@ mod tests {
         assert_eq!(factorial(3), 6);
         assert_eq!(factorial(4), 24);
         assert_eq!(factorial(5), 120);
+    }
+
+    #[test]
+    fn test_factorial_recursive() {
+        assert_eq!(factorial_recursive(0), 0);
+        assert_eq!(factorial_recursive(1), 1);
+        assert_eq!(factorial_recursive(2), 2);
+        assert_eq!(factorial_recursive(3), 6);
+        assert_eq!(factorial_recursive(4), 24);
+        assert_eq!(factorial_recursive(5), 120);
     }
 }
