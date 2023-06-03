f83d8c264e533b12f72b71b8a4a1d40402b8f328
filipecsoares <filipecarlossoares@gmail.com>
2023-06-03 14:19:02 -0300

    todo-server: add actix-web, create healthcheck endpoint

--- todo-server/src/main.rs
+++ todo-server/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/healthcheck")]
+pub async fn healthcheck() -> impl Responder {
+    HttpResponse::Ok().body("It's working!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(||{
+        App::new()
+            .service(healthcheck)
+            .default_service(web::to(||HttpResponse::NotFound()))
+    })
+    .workers(6)
+    .bind(("localhost", 4000))?
+    .run()
+    .await
+}
+
+#[actix_web::test]
+async fn not_found_route() {
+    let mut app = test::init_service(
+        App::new()
+        .service(healthcheck)
+        .default_service(web::to(|| HttpResponse::NotFound()))
+    ).await;
+
+    let req = test::TestRequest::with_uri("/crazy-path").to_request();
+
+    let resp = app.call(req).await.unwrap();
+    assert_eq!(resp.status(), StatusCode::NOT_FOUND);
 }
