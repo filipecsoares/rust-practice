55fb06876e205f1644d90b75a2ad63546e3322ea
filipecsoares <filipecarlossoares@gmail.com>
2023-06-03 19:48:34 -0300

    todo-server: ready endpoint

--- todo-server/src/main.rs
+++ todo-server/src/main.rs
@@ -1,18 +1,34 @@
-use actix_web::{get, web, test, App, HttpResponse, HttpServer, Responder};
-use actix_web::dev::Service;
-use actix_web::http::StatusCode;
+use actix_web::{get, http::StatusCode, test, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/healthcheck")]
 pub async fn healthcheck() -> impl Responder {
     HttpResponse::Ok().body("It's working!")
 }
 
+#[get("/ping")]
+pub async fn ping() -> impl Responder {
+    HttpResponse::Ok().body("pong!")
+}
+
+#[get("/~/ready")]
+pub async fn readiness() -> impl Responder {
+    let process = std::process::Command::new("sh")
+        .arg("-c")
+        .arg("echo hello")
+        .output();
+    match process {
+        Ok(_) => HttpResponse::Accepted(),
+        Err(_) => HttpResponse::InternalServerError(),
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(||{
+    HttpServer::new(|| {
         App::new()
+            .service(readiness)
             .service(healthcheck)
-            .default_service(web::to(||HttpResponse::NotFound()))
+            .default_service(web::to(|| HttpResponse::NotFound()))
     })
     .workers(6)
     .bind(("localhost", 4000))?
@@ -22,14 +38,15 @@ async fn main() -> std::io::Result<()> {
 
 #[actix_web::test]
 async fn not_found_route() {
-    let app = test::init_service(
+    let mut app = test::init_service(
         App::new()
-        .service(healthcheck)
-        .default_service(web::to(|| HttpResponse::NotFound()))
-    ).await;
+            .service(healthcheck)
+            .default_service(web::to(|| HttpResponse::NotFound())),
+    )
+    .await;
 
     let req = test::TestRequest::with_uri("/crazy-path").to_request();
 
-    let resp = app.call(req).await.unwrap();
+    let resp = test::call_service(&mut app, req).await;
     assert_eq!(resp.status(), StatusCode::NOT_FOUND);
 }
