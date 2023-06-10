5efa4fb697d4792e1b0d3283b692e71fede98ae7
filipecsoares <filipecarlossoares@gmail.com>
2023-06-10 08:14:38 -0300

    todo-server: refactor routes

--- todo-server/src/main.rs
+++ todo-server/src/main.rs
@@ -1,22 +1,16 @@
 pub mod todo_api_web;
 
-use actix_web::{web, App, HttpResponse, HttpServer};
-use todo_api_web::controller::{ping, readiness, todo::create_todo};
+use actix_web::{App, HttpServer};
+use todo_api_web::routes::app_routes;
 
 use num_cpus;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(web::scope("/api").service(create_todo))
-            .service(readiness)
-            .service(ping)
-            .default_service(web::to(|| HttpResponse::NotFound()))
-    })
-    .workers(num_cpus::get() + 2)
-    .bind(("localhost", 4004))
-    .unwrap()
-    .run()
-    .await
+    HttpServer::new(|| App::new().configure(app_routes))
+        .workers(num_cpus::get() + 2)
+        .bind(("localhost", 4004))
+        .unwrap()
+        .run()
+        .await
 }
