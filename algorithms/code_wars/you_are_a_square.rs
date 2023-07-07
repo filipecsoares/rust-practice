b868ac5bd71c0bdb4ce522d3abd0570167aa16d1
filipecsoares <filipecarlossoares@gmail.com>
2023-07-07 08:28:50 -0300

    algorithms: code wars - you are a square

---
+++ algorithms/code_wars/you_are_a_square.rs
@@ -0,0 +1,23 @@
+fn is_square(n: i64) -> bool {
+    for i in 0..=n {
+        if i * i == n {
+            return true;
+        }
+    }
+    false
+}
+
+#[cfg(test)]
+mod tests {
+    use super::is_square;
+
+    #[test]
+    fn fixed_tests() {
+        assert_eq!(is_square(-1), false, "\nYour answer (left) is not the expected answer (right).");
+        assert_eq!(is_square(0), true, "\nYour answer (left) is not the expected answer (right).");
+        assert_eq!(is_square(3), false, "\nYour answer (left) is not the expected answer (right).");
+        assert_eq!(is_square(4), true, "\nYour answer (left) is not the expected answer (right).");
+        assert_eq!(is_square(25), true, "\nYour answer (left) is not the expected answer (right).");
+        assert_eq!(is_square(26), false, "\nYour answer (left) is not the expected answer (right).");
+    }
+}
