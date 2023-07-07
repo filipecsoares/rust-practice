6756768865dc5e9d7b3d17639337b4f18412708e
filipecsoares <filipecarlossoares@gmail.com>
2023-07-07 08:37:08 -0300

    algorithms: code wars - you are a square

--- algorithms/code_wars/you_are_a_square.rs
+++ algorithms/code_wars/you_are_a_square.rs
@@ -1,10 +1,6 @@
 fn is_square(n: i64) -> bool {
-    for i in 0..=n {
-        if i * i == n {
-            return true;
-        }
-    }
-    false
+    let sqrt_n = (n as f64).sqrt() as i64;
+    return sqrt_n * sqrt_n == n;
 }
 
 #[cfg(test)]
@@ -13,11 +9,35 @@ mod tests {
 
     #[test]
     fn fixed_tests() {
-        assert_eq!(is_square(-1), false, "\nYour answer (left) is not the expected answer (right).");
-        assert_eq!(is_square(0), true, "\nYour answer (left) is not the expected answer (right).");
-        assert_eq!(is_square(3), false, "\nYour answer (left) is not the expected answer (right).");
-        assert_eq!(is_square(4), true, "\nYour answer (left) is not the expected answer (right).");
-        assert_eq!(is_square(25), true, "\nYour answer (left) is not the expected answer (right).");
-        assert_eq!(is_square(26), false, "\nYour answer (left) is not the expected answer (right).");
+        assert_eq!(
+            is_square(-1),
+            false,
+            "\nYour answer (left) is not the expected answer (right)."
+        );
+        assert_eq!(
+            is_square(0),
+            true,
+            "\nYour answer (left) is not the expected answer (right)."
+        );
+        assert_eq!(
+            is_square(3),
+            false,
+            "\nYour answer (left) is not the expected answer (right)."
+        );
+        assert_eq!(
+            is_square(4),
+            true,
+            "\nYour answer (left) is not the expected answer (right)."
+        );
+        assert_eq!(
+            is_square(25),
+            true,
+            "\nYour answer (left) is not the expected answer (right)."
+        );
+        assert_eq!(
+            is_square(26),
+            false,
+            "\nYour answer (left) is not the expected answer (right)."
+        );
     }
 }
