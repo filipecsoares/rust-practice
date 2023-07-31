a26dfb8244a465f7bb1ddd85226800f699bfcfce
filipecsoares <filipecarlossoares@gmail.com>
2023-07-31 08:13:33 -0300

    data-structure: microsoft learn rust - vec get optional matching

--- data_structure/microsoft_learn_rust/vec_get_optional.rs
+++ data_structure/microsoft_learn_rust/vec_get_optional.rs
@@ -12,4 +12,12 @@ fn main() {
     // pick the 99th item, which is non-existent:
     let non_existent = fruits.get(99);
     println!("{:?}", non_existent);
+    
+    // match
+    for &index in [0, 2, 99].iter() {
+        match fruits.get(index) {
+            Some(fruit_name) => println!("It's a delicious {}!", fruit_name),
+            None => println!("There is no fruit! :("),
+        }
+    }
 }
