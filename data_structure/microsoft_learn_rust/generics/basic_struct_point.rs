402fdbd9993bea5f7f578fe9d6485ac85ada4c73
filipecsoares <filipecarlossoares@gmail.com>
2023-08-05 07:25:28 -0300

    data-structure: microsoft learn rust - Generics - Basic Struct with same types

---
+++ data_structure/microsoft_learn_rust/generics/basic_struct_point.rs
@@ -0,0 +1,16 @@
+#[derive(Debug)]
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+fn main() {
+    let boolean = Point { x: true, y: false };
+    let integer = Point { x: 1, y: 9 };
+    let float = Point { x: 1.7, y: 4.3 };
+    let string_slice = Point { x: "high", y: "low" };
+    println!("{:?}", boolean);
+    println!("{:?}", integer);
+    println!("{:?}", float);
+    println!("{:?}", string_slice);
+}
