58a8cb0037a56227059bf418be3be86d2e17c966
filipecsoares <filipecarlossoares@gmail.com>
2024-02-24 08:52:05 -0300

    feat (guessing_game): finish guessing game from rust book

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,29 @@
-use std::io;
+use std::{cmp::Ordering, io};
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    loop {
+        println!("Please input your guess.");
+        let guess = read_guess();
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
+
+fn read_guess() -> String {
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Failed to read line");
-    println!("You guessed: {guess}");
+    guess
 }
