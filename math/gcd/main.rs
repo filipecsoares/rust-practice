bdfe7ed11a0db12415191b9668204e710443338b
filipecsoares <filipecarlossoares@gmail.com>
2025-03-04 14:58:23 -0300

    feat: math - greatest common divisor recursive and iterative

--- math/gcd/main.rs
+++ math/gcd/main.rs
@@ -1,16 +1,35 @@
 // Euclid's algorithm to find the greatest common divisor
-fn find_gcd(a: u32, b: u32) -> u32 {
+fn find_gcd_recursive(a: u32, b: u32) -> u32 {
     if b == 0 {
         return a;
     }
     let r = a % b;
-    return find_gcd(b, r);
+    return find_gcd_recursive(b, r);
+}
+
+fn find_gcd_iterative(a: u32, b: u32) -> u32 {
+    let mut a = a;
+    let mut b = b;
+    while b != 0 {
+        let r = a % b;
+        a = b;
+        b = r;
+    }
+    return a;
 }
 
 #[test]
-fn test_find_greatest_common_divisor() {
-    assert_eq(find_gcd(2, 2), 2)
-    assert_eq(find_gcd(10, 5), 5)
-    assert_eq(find_gcd(15, 20), 5)
-    assert_eq(find_gcd(5, 3), 1)
+fn test_find_greatest_common_divisor_recursive() {
+    assert_eq!(find_gcd_recursive(2, 2), 2);
+    assert_eq!(find_gcd_recursive(10, 5), 5);
+    assert_eq!(find_gcd_recursive(15, 20), 5);
+    assert_eq!(find_gcd_recursive(5, 3), 1);
+}
+
+#[test]
+fn test_find_greatest_common_divisor_iterative() {
+    assert_eq!(find_gcd_iterative(2, 2), 2);
+    assert_eq!(find_gcd_iterative(10, 5), 5);
+    assert_eq!(find_gcd_iterative(15, 20), 5);
+    assert_eq!(find_gcd_iterative(5, 3), 1);
 }
